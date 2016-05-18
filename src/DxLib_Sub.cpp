75374c6856075c879fc85e58ef19bff223194bf4
nasuton <lovetennis_airu@yahoo.co.jp>
2016-05-18 12:22:27 +0900

    キー操作を追加

--- src/DxLib_Sub.cpp
+++ src/DxLib_Sub.cpp
@@ -1,6 +1,48 @@
 #include "DxLib_Sub.h"
 
-DxLib_Sub::DxLib_Sub()
+DxLib_Sub::DxLib_Sub():
+	Now_Status(),
+	Previous_Status()
 {
 
+}
+
+void DxLib_Sub::Update_Status()
+{
+	//一時保管変数
+	char TempKey[256];
+	//全てのキーの入力情報を入手
+	GetHitKeyStateAll(TempKey);
+	//現在のキー情報を記録
+	Previous_Status = Now_Status;
+
+	//押されたキー
+	for (int i = 0; i < 256; i++)
+	{
+		if (TempKey[i] != 0)
+		{
+			Now_Status.key[i] = true;
+		}
+		else
+		{
+			Now_Status.key[i] = false;
+		}
+	}
+}
+
+bool DxLib_Sub::Push_Key(const int KEY_CODE) 
+{
+	return !Previous_Status.key[KEY_CODE] &&
+		   Now_Status.key[KEY_CODE];
+}
+
+bool DxLib_Sub::Press_Key(const int KEY_CODE)
+{
+	return Now_Status.key[KEY_CODE];
+}
+
+bool DxLib_Sub::Pull_Key(const int KEY_CODE)
+{
+	return Previous_Status.key[KEY_CODE] &&
+		   !Now_Status.key[KEY_CODE];
 }
--- src/DxLib_Sub.h
+++ src/DxLib_Sub.h
@@ -1,12 +1,26 @@
 #pragma once
 #include "DxLib.h"
 
+struct AllStatus
+{
+	bool key[256];
+};
+
 class DxLib_Sub 
 {
 public:
 	DxLib_Sub();
 
+	//情報のアップデート
+	void Update_Status();
+	//該当するキーが押された瞬間
+	bool Push_Key(const int KEY_CODE);
+	//該当するキーが押され続けているか
+	bool Press_Key(const int KEY_CODE);
+	//該当するキーが離された瞬間
+	bool Pull_Key(const int KEY__CODE);
+
 private:
-
-
+	AllStatus Now_Status;
+	AllStatus Previous_Status;
 };
